1e7d7aee720bc957c9fc794d945508c3d4bb824b
Gergo Szalai <gergo.szalai15@gmail.com>
2024-01-30 12:43:24 +0100

    Add integration between tmux and nvim

--- init.lua
+++ init.lua
@@ -110,12 +110,6 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
--- Remap moving between splits
-vim.api.nvim_set_keymap('n', '<c-h>', '<c-w>h', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<c-j>', '<c-w>j', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<c-k>', '<c-w>k', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<c-l>', '<c-w>l', {noremap = true, silent = true})
-
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -123,7 +117,7 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>E', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Highlight on yank ]]
---
+++ lua/plugins/tmux-nvim.lua
@@ -0,0 +1,5 @@
+return {
+  -- Plugin for seamless integration between tmux and nvim
+  "aserowy/tmux.nvim",
+  opts = {},
+}
